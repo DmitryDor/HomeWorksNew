1a90e59a2a6c2de0398de85318595f09c8394944
DmitryDor <dmitrydoroshenko80@gmail.com>
2021-01-26 17:41:37 +0300

    HW1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,33 +1,47 @@
 import React from "react";
+import styles from './Message.module.css'
 
 const messageData = {
-    avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
+    avatar: "https://avatarko.ru/img/kartinka/2/zhivotnye_kot_prikol_1767.jpg",
+    name: "Artem",
+    message: "Npm start нажимал?",
     time: "22:00",
 };
+
 
 function HW1() {
     return (
         <div>
-            <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
         </div>
     );
+}
+
+type MessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export const Message = (props: MessageDataType) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.image}>
+                <img src={props.avatar} alt="I'm" className={styles.photo}/>
+            </div>
+            <div className={styles.message}>
+                <div className={styles.name}>{props.name}</div>
+                <div className={styles.messageValue}>{props.message}</div>
+                <div className={styles.time}>{props.time}</div>
+            </div>
+        </div>
+    )
 }
 
 export default HW1;
